aa2ab8c756bb0298ba7fc5d515ffc9ce36e27422
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2021-09-12 13:44:18 +0200

    Improved test coverage and fixed brittleness when reading the test samples file.

--- src/Geohash.GeoRaptor.Test/GeoRaptorTests.cs
+++ src/Geohash.GeoRaptor.Test/GeoRaptorTests.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace Geohash.GeoRaptor.Tests
 {
-	public class GeoRaptorTests
+    public class GeoRaptorTests
 	{
 		HashSet<string> _geoHashes;
 
@@ -13,32 +13,75 @@ namespace Geohash.GeoRaptor.Tests
 		public void Setup()
 		{
 			var filename = "sample.csv";
-			if (!File.Exists(filename))
-			{
-				filename = "./Geohash.GeoRaptor.Test/sample.csv"; // Hack for .NET 4.8 / 4.7.x
-			}
-			_geoHashes = new HashSet<string>(File.ReadAllLines(filename));
+			var sampleFilePath = Path.Combine(TestContext.CurrentContext.TestDirectory, filename);
+			_geoHashes = new HashSet<string>(File.ReadAllLines(sampleFilePath));
 		}
 
 		[Test]
-		public void Test01()
+		public void Test_Level_5To6()
 		{
 			var result = GeoRaptor.Compress(_geoHashes, 5, 6);
 			Assert.AreEqual(414, result.Count());
 		}
 
 		[Test]
-		public void Test02()
+		public void Test_Level_4To5()
 		{
 			var result = GeoRaptor.Compress(_geoHashes, 4, 5);
 			Assert.AreEqual(50, result.Count());
 		}
 
 		[Test]
-		public void Test03()
+		public void Test_Level_3To4()
 		{
 			var result = GeoRaptor.Compress(_geoHashes, 3, 4);
 			Assert.AreEqual(5, result.Count());
+		}
+
+		[Test]
+		public void Test_Level_2To6()
+		{
+			var result = GeoRaptor.Compress(_geoHashes, 2, 6);
+			Assert.AreEqual(414, result.Count());
+		}
+
+		[Test]
+		public void Test_Level_3To4_Compare_Sets()
+		{
+			var expectedResult = new HashSet<string>
+			{
+				"w23b","w238","w21x","w21z","w21w"
+			};
+
+			var actualResult = GeoRaptor.Compress(_geoHashes, 3, 4);
+
+			Assert.IsTrue(actualResult.SetEquals(expectedResult));
+		}
+
+		[Test]
+		public void Test_Level_4To5_Compare_Sets()
+		{
+			var geohashes = new HashSet<string>
+			{
+				"w21zf9", "w21zfc", "w21zg1",
+				"w21zg3", "w21zg9", "w21zgc2",
+				"w21zu1", "w21zu3", "w21zu9",
+				"w21zuc", "w21zv1", "w21zv3",
+				"w21zv9", "w21zvc", "w21zy1",
+				"w21zy3", "w21zy9", "w21zyc",
+				"w21xy8", "w21xyb", "w21xz0"
+			};
+
+			var expectedResult = new HashSet<string> 
+			{
+				"w21zf", "w21zg", "w21zu",
+				"w21zv", "w21zy", "w21xy",
+				"w21xz" 
+			};
+
+			var actualResult = GeoRaptor.Compress(geohashes, 4, 5);
+
+			Assert.IsTrue(actualResult.SetEquals(expectedResult));
 		}
 	}
 }
