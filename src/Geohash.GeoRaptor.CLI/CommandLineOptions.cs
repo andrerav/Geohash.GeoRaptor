41bfaee35e6903c2cedc1cb9bc5943ef0ba4d056
Andreas Ravnestad <andreas.ravnestad@gmail.com>
2021-09-12 20:42:20 +0200

    Added command line functionality.

---
+++ src/Geohash.GeoRaptor.CLI/CommandLineOptions.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace Geohash.GeoRaptor.CLI
+{
+    public class CommandLineOptions
+	{
+		[Option('f', "input-file", Required = false, HelpText = "Path to a text file with geohashes (one geohash per line)")]
+		public string InputFile { get; set; }
+
+		[Option('l', "lowest-precision", Required = true, HelpText = "Lowest precision")]
+		public int MinimumPrecision { get; set; }
+
+		[Option('h', "highest-precision", Required = true, HelpText = "Highest precision")]
+		public int MaximumPrecision { get; set; }
+
+		[Option('g', "output-geometry", Required = false, HelpText = "Enable this to add WKB geometries to the output")]
+		public bool? OutputGeometry { get; set; }
+	}
+}
--- src/Geohash.GeoRaptor.CLI/Program.cs
+++ src/Geohash.GeoRaptor.CLI/Program.cs
@@ -1,13 +1,183 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using CommandLine;
+using NetTopologySuite.Geometries;
 
 namespace Geohash.GeoRaptor.CLI
 {
-	static class Program
+    static class Program
 	{
-		static void Main(string[] args)
+        static int exitCode = 0;
+        static HashSet<string> geohashes = new HashSet<string>();
+        static Lazy<Geohasher> Geohasher = new Lazy<Geohasher>((() => new Geohasher()));
+
+        static void Main(string[] args)
 		{
-			// TODO: Add CLI functionality here
-			Console.WriteLine("Hello World!");
+            CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args)
+			.WithParsed(o =>
+            {
+                ValidateArguments(o);
+
+                if (exitCode > 0) { return; }
+
+                var reader = GetInputReader(o);
+
+                ValidateReader(reader);
+
+                if (exitCode > 0) { return; }
+
+                LoadGeohashes(reader, geohashes);
+
+                var compressed = CompressGeohashes(o);
+
+                OutputCompressedGeohashes(o, compressed);
+            });
+		}
+
+        /// <summary>
+        /// Compress the geohashes using the specified precision range.
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        private static HashSet<string> CompressGeohashes(CommandLineOptions o)
+        {
+            return GeoRaptor.Compress(geohashes, o.MinimumPrecision, o.MaximumPrecision);
+        }
+
+        /// <summary>
+        /// Outputs compressed geohashes to standard output
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="compressed"></param>
+        private static void OutputCompressedGeohashes(CommandLineOptions o, HashSet<string> compressed)
+        {
+            foreach (var g in compressed)
+            {
+                if (o.OutputGeometry.HasValue && o.OutputGeometry.Value)
+                {
+                    var geometry = BoundingBox(g);
+                    Console.WriteLine(g + ";" + geometry.AsText());
+
+                }
+                else
+                {
+                    Console.WriteLine(g);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Loads geohashes from the reader and stores them in the HashSet.
+        /// It is assumed that each line contains a single geohash.
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <param name="geohashes"></param>
+        private static void LoadGeohashes(TextReader reader, HashSet<string> geohashes)
+        {
+            string geohash;
+            while ((geohash = reader.ReadLine()) != null)
+            {
+                geohashes.Add(geohash);
+            }
+        }
+
+        /// <summary>
+        /// Validate that there is input on the reader
+        /// </summary>
+        /// <param name="reader"></param>
+        private static void ValidateReader(TextReader reader)
+        {
+            if (reader == null)
+            {
+                WriteError("Unable to read input, exiting");
+                exitCode = 1;
+            }
+            else if (reader.Peek() == -1)
+            {
+                WriteError("No input detected");
+                exitCode = 1;
+            }
+        }
+
+        /// <summary>
+        /// Validates the arguments specified by the user
+        /// </summary>
+        /// <param name="o"></param>
+        private static void ValidateArguments(CommandLineOptions o)
+        {
+            if (o.MaximumPrecision > 12 || o.MaximumPrecision < 1)
+            {
+                WriteError("Maximum precision must be <= 12 and >= 1");
+                exitCode = 1;
+            }
+            else if (o.MinimumPrecision > 12 || o.MinimumPrecision < 1)
+            {
+                WriteError("Minimum precision must be <= 12 and >= 1");
+                exitCode = 1;
+            }
+            else if (o.MinimumPrecision > o.MaximumPrecision)
+            {
+                WriteError($"Minimum precision must be <= {o.MaximumPrecision}");
+                exitCode = 1;
+            }
+
+            if (!string.IsNullOrWhiteSpace(o.InputFile) && !File.Exists(o.InputFile))
+            {
+                WriteError("Input file does not exist");
+                exitCode = 1;
+            }
+        }
+
+        /// <summary>
+        /// Writes an error message to stderr
+        /// </summary>
+        /// <param name="message"></param>
+        private static void WriteError(string message)
+        {
+            Console.Error.WriteLine("Error: " + message);
+        }
+
+        /// <summary>
+        /// Returns an input reader based on whether the user has specified an input file or not
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        private static TextReader GetInputReader(CommandLineOptions o)
+        {
+            TextReader reader;
+            if (!string.IsNullOrWhiteSpace(o.InputFile))
+            {
+
+                reader = File.OpenText(o.InputFile);
+            }
+            else
+            {
+                reader = Console.In;
+            }
+
+            return reader;
+        }
+
+        /// <summary>
+        /// Converts a valid geohash to its corresponding bounding box
+        /// </summary>
+        /// <param name="geohash"></param>
+        /// <returns></returns>
+		public static Polygon BoundingBox(string geohash)
+		{
+			var bb = Geohasher.Value.GetBoundingBox(geohash);
+
+			var coordinates = new List<Coordinate>();
+			coordinates.Add(new Coordinate(bb[3], bb[0]));
+			coordinates.Add(new Coordinate(bb[3], bb[1]));
+			coordinates.Add(new Coordinate(bb[2], bb[1]));
+			coordinates.Add(new Coordinate(bb[2], bb[0]));
+			coordinates.Add(new Coordinate(bb[3], bb[0]));
+
+			var linearRing = new LinearRing(coordinates.ToArray());
+			var polygon = new Polygon(linearRing);
+			return polygon;
 		}
 	}
 }
